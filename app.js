771b7e5581ad1f0a5e7b3beb24f72dbaf8c3f04e
Somil Kaushik <somil.kaushik@somilkss297.successivetech.com>
2023-11-30 12:17:17 +0530

    Assignment-2

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import { data } from "./mockData.js";
 import cookieParser from "cookie-parser";
 
+const port = 4000;
 const app = express();
 app.use(cookieParser());
 
@@ -9,6 +10,6 @@ app.get("/", function (req, res) {
   res.send(data);
 });
 
-app.listen(4000, () => {
-  console.log("serveris running at port 4000");
+app.listen({ port }, () => {
+  console.log(`server is running at port ${port}`);
 });
