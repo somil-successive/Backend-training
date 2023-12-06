b3f0979fc0d904a2d4ad0c5a1a741cca454cc9cc
Somil Kaushik <somil.kaushik@somilkss297.successivetech.com>
2023-12-06 10:31:13 +0530

    Assignment-5

--- src/middleware/paramValidationMiddleware.js
+++ src/middleware/paramValidationMiddleware.js
@@ -1,6 +1,6 @@
 import createError from "http-errors";
 export const paramValidationMiddleware = (req, res, next) => {
-  const { id } = req.query;
+  const { id } = req.params;
   if (isNaN(id)) {
     return next(createError(406, "Not Valid Params"));
   }
