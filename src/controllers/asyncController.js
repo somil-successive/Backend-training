ab826ac90ed8bba11161ddedd3a97dfb06216b38
Somil Kaushik <somil.kaushik@somilkss297.successivetech.com>
2023-12-06 10:56:44 +0530

    Assignment-5

--- src/controllers/asyncController.js
+++ src/controllers/asyncController.js
@@ -1,15 +1,14 @@
 export const asyncData = async (req, res) => {
-  let myPromise = new Promise(function (resolve,reject) {
+  const myPromise = new Promise(function (resolve, reject) {
     setTimeout(function () {
       reject("Time Exceeds");
-    }, 3000);       
+    }, 3000);
   });
-  
-  try{
+
+  try {
     const response = await myPromise;
     console.log(response);
-  }
-  catch(err){
-    res.json({error: err})
+  } catch (err) {
+    res.json({ error: err });
   }
 };
