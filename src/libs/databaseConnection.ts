32e39b77c989c24b4442a7b78725a6618b635bf6
Somil Kaushik <somil.kaushik@somilkss297.successivetech.com>
2023-12-07 10:13:50 +0530

    Assignment-9

--- src/libs/databaseConnection.ts
+++ src/libs/databaseConnection.ts
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import playingCountries from "./countriesModel.js";
 
 class Connection {
   private mongoUri: string = "mongodb://127.0.0.1:27017/test";
@@ -9,15 +8,9 @@ class Connection {
       await mongoose.connect(this.mongoUri);
 
       console.log("MongoDB is connected successfully.");
-
-      this.seedCountries();
     } catch (err) {
       console.error("MongoDB connection error" + err);
     }
-  };
-
-  private seedCountries = async () => {
-    await playingCountries.insertMany({ country_name: "india" });
   };
 }
 export default Connection;
